05a4f9c4a07c00482ae43cca349bf9f512450890
GogaCpp <assdddffff54@mail.ru>
2024-12-22 17:37:27 +0300

    Add first lab

--- lab_1/GradientDescent.py
+++ lab_1/GradientDescent.py
@@ -1,26 +1,85 @@
 import numpy as np
 
+def gradient_descent(func, grad_func, initial_point, learning_rate, tolerance, max_iterations):
+    x, y = initial_point
+    x_history = [x]
+    y_history = [y]
+    f_history = [func(x, y)]
 
-class GradientDescent:
-    def __init__(self, function, gradient, learning_rate=0.01, max_iter=1000, tolerance=1e-6):
-        self.function = function
-        self.gradient = gradient
-        self.learning_rate = learning_rate
-        self.max_iter = max_iter
-        self.tolerance = tolerance
+    for i in range(max_iterations):
+        grad_x, grad_y = grad_func(x, y)
+        x_new = x - learning_rate * grad_x
+        y_new = y - learning_rate * grad_y
 
-    def optimize(self, initial_point):
-        point = np.array(initial_point)
-        history = [point.copy()]
+        if np.sqrt((x_new - x)**2 + (y_new - y)**2) < tolerance:
+            break
 
-        for i in range(self.max_iter):
-            grad = self.gradient(point)
-            new_point = point - self.learning_rate * grad
+        x, y = x_new, y_new
+        x_history.append(x)
+        y_history.append(y)
+        f_history.append(func(x, y))
 
-            history.append(new_point.copy())
+    return x, y, x_history, y_history, f_history
 
-            if np.linalg.norm(new_point - point) < self.tolerance:
-                break
-            point = new_point
 
-        return point, history
+def momentum_gradient_descent(func, grad_func, initial_point, learning_rate, tolerance, max_iterations,**kwarg):
+    x, y = initial_point
+    vx, vy = 0, 0 # Скорости
+    x_history = [x]
+    y_history = [y]
+    f_history = [func(x, y)]
+    momentum = kwarg["momentum"]
+
+    for i in range(max_iterations):
+        grad_x, grad_y = grad_func(x, y)
+        vx = momentum * vx - learning_rate * grad_x
+        vy = momentum * vy - learning_rate * grad_y
+        x += vx
+        y += vy
+
+        if np.sqrt((vx)**2 + (vy)**2) < tolerance: #Проверка остановки по скорости
+            break
+
+        x_history.append(x)
+        y_history.append(y)
+        f_history.append(func(x, y))
+
+    return x, y, x_history, y_history, f_history
+
+def adam(func, grad_func, initial_point, learning_rate, tolerance, max_iterations, **kwarg):
+    x, y = initial_point
+    m_x, m_y = 0, 0 # Моменты первого порядка
+    v_x, v_y = 0, 0 # Моменты второго порядка
+    x_history = [x]
+    y_history = [y]
+    f_history = [func(x, y)]
+    beta1 = kwarg["beta1"]
+    beta2 = kwarg["beta2"]
+    epsilon = kwarg["epsilon"]
+
+    for i in range(max_iterations):
+        grad_x, grad_y = grad_func(x, y)
+        m_x = beta1 * m_x + (1 - beta1) * grad_x
+        m_y = beta1 * m_y + (1 - beta1) * grad_y
+        v_x = beta2 * v_x + (1 - beta2) * grad_x**2
+        v_y = beta2 * v_y + (1 - beta2) * grad_y**2
+
+        m_x_hat = m_x / (1 - beta1**(i + 1))
+        m_y_hat = m_y / (1 - beta1**(i + 1))
+        v_x_hat = v_x / (1 - beta2**(i + 1))
+        v_y_hat = v_y / (1 - beta2**(i + 1))
+
+        x_new = x - learning_rate * m_x_hat / (np.sqrt(v_x_hat) + epsilon)
+        y_new = y - learning_rate * m_y_hat / (np.sqrt(v_y_hat) + epsilon)
+
+        if np.sqrt((x_new - x)**2 + (y_new - y)**2) < tolerance:
+            break
+
+        x, y = x_new, y_new
+        x_history.append(x)
+        y_history.append(y)
+        f_history.append(func(x, y))
+
+    return x, y, x_history, y_history, f_history
+
+
--- lab_1/main.py
+++ lab_1/main.py
@@ -1,47 +1,75 @@
-from GradientDescent import GradientDescent
 import numpy as np
-import Ploting
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from function import beale, beale_gradient, rastrigin, rastrigin_gradient, numerical_gradient
+from GradientDescent import adam, gradient_descent, momentum_gradient_descent
+
+def test_optimization(func, grad_func, initial_points, learning_rate, tolerance, max_iterations, num_runs=5, method=gradient_descent, **method_kwargs):
+    results = []
+    for initial_point in initial_points:
+        x_opt, y_opt, x_hist, y_hist, f_hist = method(func, grad_func, initial_point, learning_rate, tolerance, max_iterations, **method_kwargs)
+        results.append({'x': x_opt, 'y': y_opt, 'history': (x_hist, y_hist, f_hist)})
+
+        # Визуализация
+        fig, ax = plt.subplots()
+        X = np.arange(-5, 5, 0.1)
+        Y = np.arange(-5, 5, 0.1)
+        X, Y = np.meshgrid(X, Y)
+        Z = func(X, Y)
+        contour = ax.contour(X, Y, Z, 20)
+        point, = ax.plot([], [], 'ro')
+        def animate(i):
+            point.set_data(x_hist[:i+1], y_hist[:i+1])
+            return point,
+        ani = animation.FuncAnimation(fig, animate, frames=len(x_hist), interval=100)
+        plt.title(f'Градиентный спуск. Запуск {initial_point}')
+        plt.show()
+    return results
 
 
-def rastrigin(x):
-    A = 10
-    return A * 2 + (x[0]**2 + x[1]**2) - A * (np.cos(2 * np.pi * x[0]) + np.cos(2 * np.pi * x[1]))
+def calculate_error(results, x_analytical, y_analytical):
+    errors = []
+    for result in results:
+        x_opt = result['x']
+        y_opt = result['y']
+        error = np.sqrt((x_opt - x_analytical)*2 + (y_opt - y_analytical)*2)
+        errors.append(error)
+    return errors
 
 
-def rastrigin_grad(x):
-    A = 10
-    return np.array([2 * x[0] + A * 2 * np.pi * np.sin(2 * np.pi * x[0]),
-                     2 * x[1] + A * 2 * np.pi * np.sin(2 * np.pi * x[1])])
+initial_points = [(-0.1, 0.1)]
+learning_rate = 0.01
+tolerance = 1e-6
+max_iterations = 1000
+momentum = 0.9
+beta1 = 0.9
+beta2 = 0.999
+epsilon = 1e-8
 
+# results_rastrigin = test_optimization(rastrigin, rastrigin_gradient, initial_points, learning_rate, tolerance, max_iterations, method=gradient_descent)
 
-def rosenbrock(x):
-    a = 1
-    b = 100
-    return (a - x[0])**2 + b * (x[1] - x[0]**2)**2
+# results_rastrigin_momentum = test_optimization(
+#     rastrigin, rastrigin_gradient, initial_points,
+#     learning_rate, tolerance, max_iterations,
+#     method=adam, momentum=momentum, beta1=beta1,
+#     beta2=beta2, epsilon=epsilon
+#     )
 
+# #results_rosenbrock = test_optimization(rosenbrock, rosenbrock_gradient, initial_points, learning_rate, tolerance, max_iterations) # аналогично для Розенброка
 
-def rosenbrock_grad(x):
-    a = 1
-    b = 100
-    return np.array([-2 * (a - x[0]) - 2 * b * (x[1] - x[0]**2) * x[0],
-                     2 * b * (x[1] - x[0]**2)])
+# errors = calculate_error(results_rastrigin_momentum, 0, 0)
+# print(errors)
 
+results_beale = test_optimization(beale, beale_gradient, initial_points, learning_rate, tolerance, max_iterations, method=gradient_descent)
 
-def test_gradient_descent(function, gradient):
-    initial_point = [-5.12, 5.12]
-    gd = GradientDescent(function, gradient, learning_rate=0.1)
-    optimal_point, history = gd.optimize(initial_point)
+# results_beale_momentum = test_optimization(
+#     beale, beale_gradient, initial_points,
+#     learning_rate, tolerance, max_iterations,
+#     method=adam, momentum=momentum, beta1=beta1,
+#     beta2=beta2, epsilon=epsilon
+#     )
 
-    print("Найденная точка оптимума:", optimal_point)
-    print("Значение функции в оптимуме:", function(optimal_point))
+# #results_rosenbrock = test_optimization(rosenbrock, rosenbrock_gradient, initial_points, learning_rate, tolerance, max_iterations) # аналогично для Розенброка
 
-    # Вычисление погрешности
-    analytical_optimum = [0, 0]
-    error = np.linalg.norm(np.array(optimal_point) - np.array(analytical_optimum))
-    print("Погрешность:", error)
-
-    # Визуализация
-    Ploting.visualize_optimization(function, history)
-
-
-test_gradient_descent(rastrigin, rastrigin_grad)
+errors = calculate_error(results_beale, 0, 0)
+print(errors)
